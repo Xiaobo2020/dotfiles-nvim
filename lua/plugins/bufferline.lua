623d493e05dc88452e19e061a58282f888cfeae9
Xiaobo Wang （王晓波） <xiaobo_wang@trip.com>
2023-03-26 23:46:53 +0800

    chore: bufferline opt

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,14 +1,23 @@
 local status, bufferline = pcall(require, "bufferline")
 if (not status) then return end
 
+vim.opt.termguicolors = true
+
 bufferline.setup({
   options = {
     mode = "tabs",
     separator_style = 'slant',
+    diagnostics = "nvim_lsp",
     always_show_bufferline = false,
     show_buffer_close_icons = false,
     show_close_icon = false,
-    color_icons = true
+    color_icons = true,
+    offsets = {{
+      filetype = "NvimTree",
+      text = "File Explorer",
+      highlight = "Directory",
+      text_align = "left",
+    }}
   },
   highlights = {
     separator = {
