143b9c6ea643ead4fae32652d24af8998cd4581e
Xiaobo Wang （王晓波） <xiaobo_wang@trip.com>
2023-04-08 21:30:48 +0800

    chore: highlight cursorlineNr

---
+++ lua/config/extra.lua
@@ -0,0 +1,4 @@
+-- vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "red" })
+-- vim.api.nvim_set_hl(0, "LineNr", { fg = "green" })
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "fg", bold = true })
+-- vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "blue" })
