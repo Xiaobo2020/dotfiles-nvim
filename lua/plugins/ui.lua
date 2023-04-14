74bc207b76e2e36311dc45b3b1b1b7bd8de1df5b
Xiaobo Wang （王晓波） <xiaobo_wang@trip.com>
2023-04-14 20:46:37 +0800

    chore: update with lock

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,4 +1,37 @@
 return {
+  -- Better `vim.notify()`
+  {
+    "rcarriga/nvim-notify",
+    enabled = false,
+    keys = {
+      {
+        "<leader>un",
+        function()
+          require("notify").dismiss({ silent = true, pending = true })
+        end,
+        desc = "Delete all Notifications",
+      },
+    },
+    opts = {
+      timeout = 3000,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
+    },
+    init = function()
+      -- when noice is not enabled, install notify on VeryLazy
+      local Util = require("lazyvim.util")
+      if not Util.has("noice.nvim") then
+        Util.on_very_lazy(function()
+          vim.notify = require("notify")
+        end)
+      end
+    end,
+  },
+
   -- disable indent-blankline.nvim
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
 
