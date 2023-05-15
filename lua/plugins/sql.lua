b57c3728e6cd8fee9df477a00d42ef1e448293e1
Xiaobo Wang （王晓波） <xiaobo_wang@trip.com>
2023-05-15 09:44:03 +0800

    chore: add sql.lua

--- lua/plugins/sql.lua
+++ lua/plugins/sql.lua
@@ -21,4 +21,19 @@ return {
       },
     },
   },
+
+  -- formatter
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      table.insert(opts.ensure_installed, "sql-formatter")
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      table.insert(opts.sources, nls.builtins.formatting.sql_formatter)
+    end,
+  },
 }
