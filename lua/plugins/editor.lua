1c52301e1b25735dc4391c314bac5c22a36640a0
Xiaobo Wang （王晓波） <xiaobo_wang@trip.com>
2023-05-16 14:41:39 +0800

    chore: update lspsaga

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -154,6 +154,18 @@ return {
   {
     "glepnir/lspsaga.nvim",
     event = "LspAttach",
+    opts = {
+      ui = {
+        winblend = 10,
+        border = "rounded",
+        colors = {
+          normal_bg = "#002b36",
+        },
+      },
+      lightbulb = {
+        enable = false,
+      },
+    },
     keys = {
       { "<leader>h", "<cmd>Lspsaga hover_doc<cr>", desc = "Hover" },
       { "<leader>xc", "<cmd>Lspsaga show_cursor_diagnostics<cr>", desc = "Cursor Diagnostics (Trouble)" },
@@ -161,8 +173,8 @@ return {
       { "[q", "<cmd>Lspsaga diagnostic_jump_prev<cr>", desc = "Previous trouble/quickfix item" },
       { "]q", "<cmd>Lspsaga diagnostic_jump_next<cr>", desc = "Next trouble/quickfix item" },
     },
-    config = function()
-      require("lspsaga").setup({})
+    config = function(_, opts)
+      require("lspsaga").setup(opts)
     end,
     dependencies = {
       { "nvim-tree/nvim-web-devicons" },
