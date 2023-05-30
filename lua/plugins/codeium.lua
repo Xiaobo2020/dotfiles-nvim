174527bae38c678ca326999dd340dcd47046d4b4
Xiaobo Wang （王晓波） <xiaobo_wang@trip.com>
2023-05-30 11:25:01 +0800

    chore: add codeium

---
+++ lua/plugins/codeium.lua
@@ -0,0 +1,25 @@
+if true then
+  return {}
+end
+
+return {
+  -- Remove the `use` here if you're using folke/lazy.nvim.
+  {
+    "Exafunction/codeium.vim",
+    config = function()
+      -- Change '<C-g>' here to any keycode you like.
+      vim.keymap.set("i", "<C-g>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true })
+      vim.keymap.set("i", "<C-]>", function()
+        return vim.fn["codeium#CycleCompletions"](1)
+      end, { expr = true })
+      vim.keymap.set("i", "<C-[>", function()
+        return vim.fn["codeium#CycleCompletions"](-1)
+      end, { expr = true })
+      vim.keymap.set("i", "<C-x>", function()
+        return vim.fn["codeium#Clear"]()
+      end, { expr = true })
+    end,
+  },
+}
