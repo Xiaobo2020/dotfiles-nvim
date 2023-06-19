4b22f8c5f88bae02a4d418e2948a3f9939e23e79
Xiaobo Wang （王晓波） <xiaobo_wang@trip.com>
2023-06-19 18:30:50 +0800

    chore: update markdown preview browser

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -15,7 +15,7 @@ return {
     cmd = "MarkdownPreview",
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
-      vim.g.mkdp_browser = "Min"
+      vim.g.mkdp_browser = "Google Chrome"
       vim.g.mkdp_markdown_css = "~/.config/nvim/colors/markdown.css"
       vim.g.mkdp_page_title = "${name}"
       vim.g.mkdp_preview_options = { hide_yaml_meta = 1, disable_filename = 1 }
