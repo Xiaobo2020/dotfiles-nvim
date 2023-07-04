28360ff52ae32bd23a8754e2c4eef615202ed18c
Xiaobo Wang （王晓波） <xiaobo_wang@trip.com>
2023-07-04 15:26:08 +0800

    chore: update bookmarks

--- lua/plugins/bookmarks.lua
+++ lua/plugins/bookmarks.lua
@@ -1,68 +1,82 @@
 return {
   {
-    "tomasky/bookmarks.nvim",
-    branch = "main",
+    "crusj/bookmarks.nvim",
+    opts = {
+      preview_ext_enable = true,
+      fix_enable = true,
+      virt_text = "",
+      virt_pattern = {},
+    },
     keys = {
       {
         "<leader>fm",
-        "<Cmd>Telescope bookmarks list<CR>",
+        "<Cmd>Telescope bookmarks<CR>",
         desc = "Find All Bookmarks",
       },
       {
-        "<leader>mm",
+        "<leader>ma",
         function()
-          require("bookmarks").bookmark_toggle()
+          require("bookmarks").add_bookmarks()
         end,
-        desc = "Bookmark toggle",
-      },
-      {
-        "<leader>mi",
-        function()
-          require("bookmarks").bookmark_ann()
-        end,
-        desc = "Bookmark edit annotation",
-      },
-      {
-        "<leader>mc",
-        function()
-          require("bookmarks").bookmark_clean()
-        end,
-        desc = "Bookmark clean",
+        desc = "Bookmark add",
       },
       {
         "<leader>ml",
         function()
-          require("bookmarks").bookmark_list()
+          require("bookmarks").toggle_bookmarks()
         end,
         desc = "Bookmark list",
       },
     },
-    config = function()
-      require("bookmarks").setup()
+    branch = "main",
+    dependencies = { "nvim-web-devicons" },
+    config = function(_, opts)
+      require("bookmarks").setup(opts)
       require("telescope").load_extension("bookmarks")
     end,
   },
 
   -- {
-  --   "crusj/bookmarks.nvim",
-  --   keys = {
-  --     { "<tab><tab>", mode = { "n" } },
-  --   },
+  --   "tomasky/bookmarks.nvim",
   --   branch = "main",
-  --   dependencies = { "nvim-web-devicons" },
+  --   keys = {
+  --     {
+  --       "<leader>fm",
+  --       "<Cmd>Telescope bookmarks list<CR>",
+  --       desc = "Find All Bookmarks",
+  --     },
+  --     {
+  --       "<leader>mm",
+  --       function()
+  --         require("bookmarks").bookmark_toggle()
+  --       end,
+  --       desc = "Bookmark toggle",
+  --     },
+  --     {
+  --       "<leader>mi",
+  --       function()
+  --         require("bookmarks").bookmark_ann()
+  --       end,
+  --       desc = "Bookmark edit annotation",
+  --     },
+  --     {
+  --       "<leader>mc",
+  --       function()
+  --         require("bookmarks").bookmark_clean()
+  --       end,
+  --       desc = "Bookmark clean",
+  --     },
+  --     {
+  --       "<leader>ml",
+  --       function()
+  --         require("bookmarks").bookmark_list()
+  --       end,
+  --       desc = "Bookmark list",
+  --     },
+  --   },
   --   config = function()
   --     require("bookmarks").setup()
   --     require("telescope").load_extension("bookmarks")
   --   end,
-  -- },
-
-  -- {
-  --   "telescope.nvim",
-  --   dependencies = {
-  --     "crusj/bookmarks.nvim",
-  --     config = function()
-  --       require("telescope").load_extension("bookmarks")
-  --     end,
-  --   },
   -- },
 }
