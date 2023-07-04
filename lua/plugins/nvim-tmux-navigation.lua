0b1f33f837f65f5c494c0c470795cf18849f68f1
Xiaobo Wang （王晓波） <xiaobo_wang@trip.com>
2023-07-04 23:39:37 +0800

    chore: nvim-tmux-navigation

---
+++ lua/plugins/nvim-tmux-navigation.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "alexghergh/nvim-tmux-navigation",
+    opts = {
+      disable_when_zoomed = true,
+      keybindings = {
+        left = "<C-h>",
+        down = "<C-j>",
+        up = "<C-k>",
+        right = "<C-l>",
+        last_active = "<C-\\>",
+        next = "<C-Space>",
+      },
+    },
+    config = function(_, opts)
+      require("nvim-tmux-navigation").setup(opts)
+    end,
+  },
+}
