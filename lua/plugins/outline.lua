ee1451ad16d5bed0486e080d0b442fd08e944f19
Xiaobo Wang （王晓波） <xiaobo_wang@trip.com>
2023-07-04 15:40:58 +0800

    chore: outline

---
+++ lua/plugins/outline.lua
@@ -0,0 +1,10 @@
+return {
+
+  -- add symbols-outline
+  {
+    "simrat39/symbols-outline.nvim",
+    cmd = "SymbolsOutline",
+    keys = { { "<leader>uo", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    config = true,
+  },
+}
