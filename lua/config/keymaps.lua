a1bc35ea4e74294f4c91d936c5eac24e3865869d
Xiaobo Wang （王晓波） <xiaobo_wang@trip.com>
2023-07-05 10:23:21 +0800

    chore: update keymaps for tmux-navigator

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -21,3 +21,10 @@ map("i", "jk", "<ESC>", { desc = "ESC", silent = true, noremap = true })
 
 -- LSP Finder
 map("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", { desc = "LSP Finder", silent = true, noremap = true })
+
+-- Tmux Navigator
+map("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>", { desc = "Navigate Left", silent = true, noremap = true })
+map("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>", { desc = "Navigate Right", silent = true, noremap = true })
+map("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>", { desc = "Navigate Down", silent = true, noremap = true })
+map("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>", { desc = "Navigate Up", silent = true, noremap = true })
+map("n", "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>", { desc = "Navigate Previous", silent = true, noremap = true })
--- lua/plugins/tmux-navigation.lua
+++ lua/plugins/tmux-navigation.lua
@@ -1,6 +1,13 @@
 return {
   {
     "christoomey/vim-tmux-navigator",
+    -- keys = {
+    --   { "<C-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Navigate Left" },
+    --   { "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Navigate Right" },
+    --   { "<C-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Navigate Down" },
+    --   { "<C-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Navigate Up" },
+    --   { "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>", desc = "Navigate Previous" },
+    -- },
     event = "VeryLazy",
   },
 }
