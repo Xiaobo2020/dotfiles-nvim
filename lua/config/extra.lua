610e34ec91d47955b9137f672b8d1d8f53afaef0
Xiaobo Wang （王晓波） <xiaobo_wang@trip.com>
2023-07-11 15:55:09 +0800

    chore: update sign color

--- lua/config/extra.lua
+++ lua/config/extra.lua
@@ -1,4 +1,9 @@
--- vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "red" })
--- vim.api.nvim_set_hl(0, "LineNr", { fg = "green" })
-vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "fg", bold = true })
--- vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "blue" })
+-- Sets colors to line numbers Above, Current and Below  in this order
+function LineNumberColors()
+  vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#829289", bold = false })
+  vim.api.nvim_set_hl(0, "LineNr", { fg = "#829289", bold = false })
+  vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#DBBC7F", bold = true })
+  vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#829289", bold = false })
+end
+
+LineNumberColors()
