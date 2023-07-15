ef2ce184dd09020e56b95c9d306e68a5c6581aba
Xiaobo Wang （王晓波） <xiaobo_wang@trip.com>
2023-07-16 00:34:52 +0800

    chore: update LineNr

--- lua/config/extra.lua
+++ lua/config/extra.lua
@@ -1,7 +1,7 @@
 -- Sets colors to line numbers Above, Current and Below  in this order
 function LineNumberColors()
   vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#829289", bold = false })
-  vim.api.nvim_set_hl(0, "LineNr", { fg = "#829289", bold = false })
+  -- vim.api.nvim_set_hl(0, "LineNr", { fg = "#829289", bold = false })
   vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#DBBC7F", bold = true })
   vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#829289", bold = false })
 end
