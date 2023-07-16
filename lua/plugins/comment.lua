1b5787dd7a31a60f930f9262dd80ffcef7cc0263
Xiaobo Wang （王晓波） <xiaobo_wang@trip.com>
2023-07-16 19:35:59 +0800

    chore: split coding.lua

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,12 +1,5 @@
 return {
   {
-    "windwp/nvim-ts-autotag",
-    config = function()
-      require("nvim-ts-autotag").setup()
-    end,
-  },
-
-  {
     "numToStr/Comment.nvim",
     dependencies = {
       "JoosepAlviste/nvim-ts-context-commentstring",
